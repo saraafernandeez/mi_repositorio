2fe35114e90305cf0b77a69b65a715cdb3bfd877
Sara <Sara@192.168.1.34>
2021-09-21 19:06:05 +0200

    Segundo cambio

--- Ejercicios_BaseProgramacion_Ej11/src/paquete/Ejecuta.java
+++ Ejercicios_BaseProgramacion_Ej11/src/paquete/Ejecuta.java
@@ -3,8 +3,14 @@ package paquete;
 public class Ejecuta {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/*
+			11. Programa que lee nombre y año de nacimiento de una serie de alumnos hasta introducir
+			como nombre de alumno “fin”. En ese momento mostrará la edad media de los alumnos y
+			el nombre del alumno menor. NOTA: Al introducir como nombre “fin”, no nos ha de pedir el
+			año de nacimiento.
+		*/
+		
+		
 	}
 
 }
