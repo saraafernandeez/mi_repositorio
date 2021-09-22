54b567e01b850ba0dfdf05f7ed5b5788319aafce
Sara <Sara@192.168.1.34>
2021-09-22 12:43:32 +0200

    SARA 22-09-2021 12:43

--- Ejercicios_BaseProgramacion_Ej11/src/paquete/Ejecuta.java
+++ Ejercicios_BaseProgramacion_Ej11/src/paquete/Ejecuta.java
@@ -14,8 +14,8 @@ public class Ejecuta {
 		*/
 		
 		String nombre;
-		String nombreMenor = null;
-		String mensaje = null;
+		String nombreMenor = "";
+		String mensaje = "";
 		int anoNacimiento;
 		int edad;
 		int suma = 0; // acumulador
@@ -24,43 +24,36 @@ public class Ejecuta {
 		int contEdad = 0; // contador
 		Scanner sc = new Scanner(System.in);
 		int anoActual = Calendar.getInstance().get(Calendar.YEAR);
+				
+		System.out.println("Introduzca nombre: ");
+		nombre = sc.nextLine();
 		
-		boolean continuar = true;
-		
-		do
+		while(nombre.equals("fin")==false)
 		{
-			System.out.println("Introduzca nombre: ");
-			nombre = sc.nextLine();
+			System.out.println("Introduzca el año de nacimiento: ");
+			anoNacimiento = Integer.valueOf( sc.nextLine() );
 			
-			if(nombre.equals("fin"))
+			edad = anoActual - anoNacimiento;
+			contEdad++; // contEdad = contEdad + 1
+			suma += edad; //suma = suma + edad;
+				
+			if(edad < menor)
 			{
-				continuar = false;
+				menor = edad;
+				nombreMenor = nombre;
 			}
-			else
-			{
-				System.out.println("Introduzca el año de nacimiento: ");
-				anoNacimiento = Integer.valueOf( sc.nextLine() );
-				
-				edad = anoActual - anoNacimiento;
-				
-				suma += edad; //suma = suma + edad;
-				
-				if(edad < menor)
-				{
-					menor = edad;
-					nombreMenor = nombre;
-				}
-			}
+			
+			System.out.println("Introduzca nombre: ");
+			nombre = sc.nextLine();	
 		}
-		while(continuar); // Mientras el nombre no sea igual a "fin"
 		
 		sc.close();
 		
 		if(contEdad > 0)
 		{
 			media = (float)suma/ (float)contEdad;
-			mensaje = "El más joven tiene se llama " + nombreMenor + ", y la media de edad es " + media + ".";
+			mensaje = "La persona más joven se llama " + nombreMenor + ", y la media de edad es " + media + ".";
+			System.out.println(mensaje);
 		}
-		System.out.println(mensaje);
 	}
 }
